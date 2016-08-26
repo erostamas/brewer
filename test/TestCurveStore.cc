3f1f80ab17376c906eec9b4bb5558742ecf4795a
Eros Tamas <tamas.eros1@gmail.com>
2016-08-26 13:39:41 +0200

    Added new testcase to curvestore test

--- test/TestCurveStore.cc
+++ test/TestCurveStore.cc
@@ -4,8 +4,16 @@
 
 BOOST_FIXTURE_TEST_SUITE(curve_store_test_suite, CurveStore)
 
-BOOST_AUTO_TEST_CASE(hello) {
-    BOOST_CHECK(true);
+BOOST_AUTO_TEST_CASE(test_add_curve_from_string) {
+    addCurve("curve1", "1:2;3:4;5:6");
+    BOOST_CHECK_EQUAL(1, getSize());
+    BOOST_CHECK_EQUAL("curves: curve1", getCurveNames());
+    BOOST_CHECK_EQUAL(1, _curves["curve1"][0].get()->getSetpoint());
+    BOOST_CHECK_EQUAL(2, _curves["curve1"][0].get()->getDuration());
+    BOOST_CHECK_EQUAL(3, _curves["curve1"][1].get()->getSetpoint());
+    BOOST_CHECK_EQUAL(4, _curves["curve1"][1].get()->getDuration());
+    BOOST_CHECK_EQUAL(5, _curves["curve1"][2].get()->getSetpoint());
+    BOOST_CHECK_EQUAL(6, _curves["curve1"][2].get()->getDuration());
 }
 
 BOOST_AUTO_TEST_CASE(testcase1) {
